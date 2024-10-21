968551129d09d09f2b6b68bcb84594a0fb9fbb89
RachelBarume <barume.r@northeastern.edu>
2024-10-21 19:02:23 -0400

    Revert "updating new resume"
    This reverts commit ce214ebbc27ebb8cc3da662bc028c26e9f21fde6.

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
-
-//import Navbar from "./components/Navbar";
-import index from "./public/index";
-import Apps from "./components/Apps";
-import Styles from "./components/Styles";
-//import Image from "next/image";
-//import AboutSection from "./components/AboutSection";
-//import Experience from "./components/Experience";
-//import ProjectSection from "./components/ProjectSection";
-//import ContactMe from "./components/Contact";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Image from "next/image";
+import AboutSection from "./components/AboutSection";
+import Experience from "./components/Experience";
+import ProjectSection from "./components/ProjectSection";
+import ContactMe from "./components/Contact";
 
 
 
@@ -16,10 +13,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar/>
       <div class= "container mt-24 mx-auto px-12 py-4">
-        <Apps/>
-        <Styles/>
-        <index/>
-        
+        <HeroSection/>
+        <AboutSection/>
+        <Experience/>
+        <ProjectSection/>
+        <ContactMe/>
       </div>
     </main>
   );
