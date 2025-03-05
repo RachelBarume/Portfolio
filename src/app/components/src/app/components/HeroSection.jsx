db7648b57da4209f7ca501b1d167943d5f93db7e
RachelBarume <barume.r@northeastern.edu>
2025-03-05 15:53:44 -0500

    Updating Resume

--- src/app/components/src/app/components/HeroSection.jsx
+++ src/app/components/src/app/components/HeroSection.jsx
@@ -5,8 +5,7 @@ import Image from "next/image";
 import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
-
-  // function to handle scrolling to Contact Me section
+  // Function to handle scrolling to Contact Me section
   const scrollToContactMe = () => {
     const contactMeSection = document.getElementById('contact-me');
     if (contactMeSection) {
@@ -19,8 +18,8 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className= "text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-500 ">Hello, I'm {" "}</span>
-            <br></br>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-500">Hello, I'm {" "}</span>
+            <br />
             <TypeAnimation
               sequence={[
                 "Rachel",
@@ -38,26 +37,22 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#C7C2D0] text-base sm:text-lg mb-6 lg:text-xl">
-          I am a third-year Computer Engineering student pursuing a path in technology.
-          I am currently working as a Software Developer Engineer Co-op at Amazon Robotics, 
-          I have over six months of full-time work experience as a Developer, along with numerous personal project experiences.
+            I am a third-year Computer Engineering student pursuing a path in technology.
+            I am currently working as a Software Developer Engineer Co-op at Amazon Robotics. 
+            I have over six months of full-time work experience as a Developer, along with numerous personal project experiences.
           </p>
           <div>
             <button 
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-white via-blue-400 to-purple-700 hover:bg-slate-200 text-white"
-<<<<<<< HEAD:src/app/components/src/app/components/HeroSection.jsx
               onClick={() => window.open('https://drive.google.com/file/d/1I8Jp5jyWh--eXaxctQ4AK3OS8JOV2vYb/view?usp=sharing', '_blank')}
-=======
-              onClick={() => window.open('https://drive.google.com/file/d/1sqZ-feCMG_CNgV8STfgnJ_a9VJGUiM2Z/view?usp=sharing', '_blank')}
->>>>>>> f042747b531471639a9711f1fe7450eda81832bd:src/app/components/HeroSection.jsx
             >
-              <span className="block bg-[#121212} hover:bg-slate-800 rounded-full px-5 py-2"> Access Resume </span>
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"> Access Resume </span>
             </button>
             <button 
               className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-white via-blue-400 to-purple-700 hover:bg-slate-800 text-white mt-3"
               onClick={scrollToContactMe}
             > 
-              <span className="block bg-[#121212} hover:bg-slate-800 rounded-full px-5 py-2"> Contact Me</span> 
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"> Contact Me</span> 
             </button>
           </div>
         </div>
